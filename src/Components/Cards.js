bf13b134e7fd96f862c05753453f2acf2aa51a83
RealShivesh <realshivesh@gmail.com>
2021-05-30 16:06:33 +0530

    Final UI done

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import './Cards.css'
+import EmailIcon from '@material-ui/icons/Email'
+import PersonIcon from '@material-ui/icons/Person'
 
 const Cards = ({ users }) => {
     return (
@@ -9,8 +11,8 @@ const Cards = ({ users }) => {
                     <center>
                     <div key={users.id} className="card">
                     <center><img src={users.avatar} alt=" " /></center>
-                    <h1 key={users.id}>{users.first_name} {users.last_name}</h1>
-                        <h2>{users.email}</h2>
+                    <h1 key={users.id}><EmailIcon/> : {users.first_name} {users.last_name}</h1>
+                        <h2><PersonIcon/> : {users.email}</h2>
                         </div>
                         </center>
             </>)}
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import './NavBar.css'
 
+
 function NavBar({getUsers}) {
     return (
         <div>
-            <div className="navbar">BrandName</div>
-            <button onClick={getUsers}>GET USERS</button>
-            <br/>
-        Page 1 |
-        Page 2</div>
+            <div className="navbar">BrandName<br/>
+            <center><button onClick={getUsers}>GET USERS</button></center>
+            </div></div>
     )
 }
 
